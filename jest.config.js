8a6a64798173785a1d4b0555b59f8990815858ec
gapcomputer <gapcomputer@users.noreply.github.com>
2025-05-23 03:22:10 +0000

    Add Jest configuration file

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testMatch: ['**/__tests__/**/*.test.js'],
+    verbose: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov']
+};
