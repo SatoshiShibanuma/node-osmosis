00ac81a297ddd1294ed8f76a11ae036e46aefb59
Hattrix3 <Hattrix3@users.noreply.github.com>
2025-05-23 02:16:17 +0000

    Update fetch tests for compatibility

--- test/fetch.js
+++ test/fetch.js
@@ -1,20 +1,18 @@
-'use strict';
-
 const assert = require('assert');
 const osmosis = require('../index');
 
-describe('Fetch Command', function() {
-    it('should define a fetch method', function() {
+describe('Fetch Command', () => {
+    it('should define a fetch method', () => {
         assert.equal(typeof osmosis.fetch, 'function', 'Fetch method is not defined');
     });
 
-    it('should require a string URL', function() {
+    it('should require a string URL', () => {
         assert.throws(() => {
             osmosis.fetch(123);
         }, /URL must be a string/, 'Did not throw error for non-string URL');
     });
 
-    it('should create a chainable command', function() {
+    it('should create a chainable command', () => {
         const instance = osmosis.fetch('https://example.com');
         assert.equal(typeof instance, 'function', 'Fetch did not return a chainable command');
     });
